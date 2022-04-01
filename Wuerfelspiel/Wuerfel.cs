d08a429fc8cb38b295d2812f98f2c13e49337d62
Bernd Elbel <wiesor777@gmail.com>
2022-04-01 11:56:36 +0200

    Wuerfel Klasse implementiert

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,5 +4,62 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
+        private int anzahlSeiten;
+        private int letzteErgebnis;
+        private bool gesichert;
+        private Random rnd;
+
+        public Wuerfel ()
+        {
+        }
+        public Wuerfel (int anzahlSeiten)
+        {
+        }
+
+        public int AnzahlSeiten
+        {
+            get
+            {
+                return anzahlSeiten;
+            }
+        }
+
+        public int LetztesErgebnis
+        {
+            get
+            {
+                return letzteErgebnis;
+            }
+        }
+
+        public bool Gesichert
+        {
+            get
+            {
+                return gesichert;
+            }
+            set
+            {
+                this.Gesichert = gesichert;
+            }
+        }
+
+        public void SicherungUmschalten ()
+        {
+            if (Gesichert == true)
+            {
+                Gesichert = false;
+            }
+            else
+            {
+                Gesichert = true;
+            }
+        }
+
+        public int Wuerfeln()
+        {
+            letzteErgebnis = rnd.Next(1, 6);
+            return letzteErgebnis;
+        }
     }
 }
