037536bb59b3b3cac7036a9a2953384bf5878235
Bernd Elbel <wiesor777@gmail.com>
2022-04-08 12:24:52 +0200

    Becher Klasse hinzugefügt

---
+++ Wuerfelspiel/Becher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wuerfelspiel
+{
+    class Becher:Wuerfel
+    {
+        private Wuerfel[] wuerfel = new Wuerfel[5];
+        private int restlicheVersuche;
+
+        public Becher(Wuerfel wuerfel)
+        {
+        }
+
+        public Wuerfel[] Wuerfel
+        {
+            get
+            {
+                return wuerfel;
+            }
+        }
+        public void WuerfelSicherungUmschalten()
+        {
+            SicherungUmschalten();
+        }
+
+        public void WuerfelWuerfeln()
+        {
+            Wuerfeln();
+        }
+    }
+}
