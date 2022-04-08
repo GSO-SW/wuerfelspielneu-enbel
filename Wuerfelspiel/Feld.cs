fca36a9f8095af6dc42f0a47a3ddd1b0b68ad1e3
Bernd Elbel <wiesor777@gmail.com>
2022-04-08 12:55:58 +0200

    Kniffel Klasse, Block Klasse,  und Feld Klasse eingefügt

---
+++ Wuerfelspiel/Feld.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wuerfelspiel
+{
+    class Feld:Block
+    {
+        private int akzeptierteAugenzahl;
+        private int wert;
+
+        public Feld(int akzeptierteAugenzahl)
+        {
+            this.akzeptierteAugenzahl = akzeptierteAugenzahl;
+        }
+
+        public int AkzeptierteAugenzahl
+        {
+            get
+            {
+                return akzeptierteAugenzahl;
+            }
+        }
+
+        public int Wert
+        {
+            get
+            {
+                return wert;
+            }
+        }
+
+        public int Eintragen(Wuerfel[])
+        {
+            
+        }
+    }
+}
